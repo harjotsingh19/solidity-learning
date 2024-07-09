fb896c990472e7b547283833a8ef553aef7737ff
harjot-debut <harjot.singh@debutinfotech.com>
2024-07-09 17:58:43 +0530

    added new programs

--- 7_1_if-else-continue-break-with-array.sol
+++ 7_1_if-else-continue-break-with-array.sol
@@ -6,12 +6,13 @@ contract arrayExample {
     uint[] arr;
 
     function dynamicArray() public{
-         arr = new uint[](5);  
+         arr = new uint[](5);   //This line initializes the array arr with 5 elements, all set to 0. So initially, arr is [0, 0, 0, 0, 0].
 
         for(uint i=0;i<10;i++){
         
             if(i==2){
-                arr.push(6);
+                arr.push(6);  //arr.push(6);: This adds the element 6 to the end of the array, making arr [0, 0, 0, 0, 0, 6].
+                                //arr.push(7);: This adds the element 7 to the end of the array, making arr [0, 0, 0, 0, 0, 6, 7].
                 arr.push(7);
                 continue;
                  
